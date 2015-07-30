c880f0fc83579caae37e0645699d207b3ed9a1f5
Cedric Hurst <cedric@spantree.net>
2015-07-30 17:14:54 -0500

    waiting for cloud init to finish before installing ruby

---
+++ system/scripts/wait-for-cloud-init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o xtrace  # trace what gets executed
+set -o errexit # exit when a command fails.
+set -o nounset # exit when your script tries to use undeclared variables
+
+# this is a temporary workaround
+if [ -d /var/lib/cloud ]; then
+    while [ ! -f /var/lib/cloud/instance/boot-finished ] ; do
+        sleep 10
+        echo "sleeping for 10 seconds while cloud-init is running"
+    done
+    while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
+        sleep 10
+        echo "Waiting while apt is ran by cloud-init"
+    done
+fi
