825c9dfd60d855d818b56df3019993ef3e627004
Cedric Hurst <cedric@spantree.net>
2015-07-30 00:15:46 -0500

    migrating the apt-get clean stuff to ruby installer

--- system/scripts/install-rails.sh
+++ system/scripts/install-rails.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o xtrace  # trace what gets executed
+set -o errexit # exit when a command fails.
+set -o nounset # exit when your script tries to use undeclared variables
 
 VERSION=4.2.3
 
@@ -9,11 +13,7 @@ BUNDLE_APP_CONFIG=$GEM_HOME
 mkdir -p /var/cache/install-flags/rails
 
 if [ ! -f "/var/cache/install-flags/rails/${VERSION}b" ]; then
-	apt-get clean \
-	&& mv /var/lib/apt/lists /var/lib/apt/lists.old_`date '+%Y%m%d_%H%M%S'` \
-	&& mkdir -p /var/lib/apt/lists/partial \
-    && apt-get clean \
-	&& apt-get update \
+	apt-get update \
 	&& apt-get install -y nodejs libpq-dev libsqlite3-dev --no-install-recommends \
 	&& rm -rf /var/lib/apt/lists/* \
 	&& gem install rails --version "$VERSION" \
--- system/scripts/install-ruby.sh
+++ system/scripts/install-ruby.sh
@@ -6,7 +6,11 @@ DOWNLOAD_SHA256=f22a6447811a81f3c808d1c2a5ce3b5f5f0955c68c9a749182feb425589e6635
 
 mkdir -p /var/cache/install-flags/ruby
 if [ ! -f "/var/cache/install-flags/ruby/${VERSION}" ]; then
-	apt-get update \
+	apt-get clean \
+	&& mv /var/lib/apt/lists /var/lib/apt/lists.old_`date '+%Y%m%d_%H%M%S'` \
+	&& mkdir -p /var/lib/apt/lists/partial \
+    && apt-get clean \
+	&& apt-get update
 	&& apt-get install -y bison libgdbm-dev ruby autoconf zlib1g-dev libreadline-dev libssl-dev \
 	&& rm -rf /var/lib/apt/lists/* \
 	&& mkdir -p /usr/src/ruby \
