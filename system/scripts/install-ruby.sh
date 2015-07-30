ac28c68973d59c9ae0e612c9685bac1bf21346a9
Cedric Hurst <cedric@spantree.net>
2015-07-30 10:22:54 -0500

    removing rubygems install and apt-clean stuff
    shouldn't be needed for trusty

--- system/scripts/install-ruby.sh
+++ system/scripts/install-ruby.sh
@@ -10,12 +10,8 @@ DOWNLOAD_SHA256=f22a6447811a81f3c808d1c2a5ce3b5f5f0955c68c9a749182feb425589e6635
 
 mkdir -p /var/cache/install-flags/ruby
 if [ ! -f "/var/cache/install-flags/ruby/${VERSION}" ]; then
-	apt-get clean \
-	&& mv /var/lib/apt/lists /var/lib/apt/lists.old_`date '+%Y%m%d_%H%M%S'` \
-	&& mkdir -p /var/lib/apt/lists/partial \
-    && apt-get clean \
-	&& apt-get update \
-	&& apt-get install -y bison libgdbm-dev ruby rubygems autoconf zlib1g-dev libreadline-dev libssl-dev \
+	apt-get update \
+	&& apt-get install -y bison libgdbm-dev ruby autoconf zlib1g-dev libreadline-dev libssl-dev \
 	&& rm -rf /var/lib/apt/lists/* \
 	&& mkdir -p /usr/src/ruby \
 	&& curl -fSL -o ruby.tar.gz "http://cache.ruby-lang.org/pub/ruby/${MAJOR_VERSION}/ruby-${VERSION}.tar.gz" \
