b836f11f1e1fa680a50e2634091c32b58872fb1b
Cedric Hurst <cedric@spantree.net>
2015-07-30 14:27:35 -0500

    switching to install rbenv via apt-get

--- system/scripts/install-ruby.sh
+++ system/scripts/install-ruby.sh
@@ -4,27 +4,21 @@ set -o xtrace  # trace what gets executed
 set -o errexit # exit when a command fails.
 set -o nounset # exit when your script tries to use undeclared variables
 
-MAJOR_VERSION=2.1
 VERSION=2.1.2
-DOWNLOAD_SHA256=f22a6447811a81f3c808d1c2a5ce3b5f5f0955c68c9a749182feb425589e6635
+BASH_PROFILE="${HOME}/.bash_profile"
+touch $BASH_PROFILE
 
 mkdir -p /var/cache/install-flags/ruby
 if [ ! -f "/var/cache/install-flags/ruby/${VERSION}" ]; then
-	apt-get update \
-	&& apt-get install -y bison libgdbm-dev make ruby-dev rubygems-integration autoconf zlib1g-dev libreadline-dev libssl-dev \
-	&& rm -rf /var/lib/apt/lists/* \
-	&& mkdir -p /usr/src/ruby \
-	&& curl -fSL -o ruby.tar.gz "http://cache.ruby-lang.org/pub/ruby/${MAJOR_VERSION}/ruby-${VERSION}.tar.gz" \
-	&& echo "${DOWNLOAD_SHA256} *ruby.tar.gz" | sha256sum -c - \
-	&& tar -xzf ruby.tar.gz -C /usr/src/ruby --strip-components=1 \
-	&& rm ruby.tar.gz \
-	&& cd /usr/src/ruby \
-	&& autoconf \
-	&& ./configure --disable-install-doc \
-	&& make -j"$(nproc)" \
-	&& make install \
-	&& apt-get purge -y --auto-remove bison ruby autoconf \
-	&& rm -r /usr/src/ruby \
+    apt-get clean \
+	&& mv /var/lib/apt/lists /var/lib/apt/lists.old_`date '+%Y%m%d_%H%M%S'` \
+	&& mkdir -p /var/lib/apt/lists/partial \
+    && apt-get clean \
+	&& apt-get update \
+	&& apt-get install -y git-core curl ruby-dev rbenv zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libpq-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+	
+	rbenv install -v "${VERSION}" \
+	&& rbenv global "${VERSION}" \
 	&& touch "/var/cache/install-flags/ruby/${VERSION}"
 fi
 
