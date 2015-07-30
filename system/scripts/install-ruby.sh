82c1dba479d5ad1d27aba4ad3c16ab4f9025547a
Cedric Hurst <cedric@spantree.net>
2015-07-30 00:19:00 -0500

    fixing missing line continuation

--- system/scripts/install-ruby.sh
+++ system/scripts/install-ruby.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o xtrace  # trace what gets executed
+set -o errexit # exit when a command fails.
+set -o nounset # exit when your script tries to use undeclared variables
 
 MAJOR_VERSION=2.1
 VERSION=2.1.2
@@ -10,7 +14,7 @@ if [ ! -f "/var/cache/install-flags/ruby/${VERSION}" ]; then
 	&& mv /var/lib/apt/lists /var/lib/apt/lists.old_`date '+%Y%m%d_%H%M%S'` \
 	&& mkdir -p /var/lib/apt/lists/partial \
     && apt-get clean \
-	&& apt-get update
+	&& apt-get update \
 	&& apt-get install -y bison libgdbm-dev ruby autoconf zlib1g-dev libreadline-dev libssl-dev \
 	&& rm -rf /var/lib/apt/lists/* \
 	&& mkdir -p /usr/src/ruby \
