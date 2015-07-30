5f21ee2ac3d00c7197d423c35f0572e9f83b1f05
Cedric Hurst <cedric@spantree.net>
2015-07-30 18:01:33 -0500

    only touching batch profile if ruby not install

--- system/scripts/install-ruby.sh
+++ system/scripts/install-ruby.sh
@@ -6,11 +6,11 @@ set -o nounset # exit when your script tries to use undeclared variables
 
 VERSION=2.1.2
 BASH_PROFILE="${HOME}/.bash_profile"
-touch $BASH_PROFILE
 
 mkdir -p /var/cache/install-flags/ruby
 if [ ! -f "/var/cache/install-flags/ruby/${VERSION}" ]; then
-	apt-get update \
+	touch $BASH_PROFILE \
+	&& apt-get update \
 	&& apt-get install -y git-core curl ruby-dev rbenv zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libpq-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 	
 	rbenv install -v "${VERSION}" \
