8c709f1004482d1444014d46a82c6d404022af59
Cedric Hurst <cedric@spantree.net>
2015-07-30 09:57:07 -0500

    adding trace outputs to provisioning scripts

--- system/scripts/install-app.sh
+++ system/scripts/install-app.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o xtrace  # trace what gets executed
+set -o errexit # exit when a command fails.
+set -o nounset # exit when your script tries to use undeclared variables
 
 GEM_HOME=/usr/local/bundle
 PATH=$GEM_HOME/bin:$PATH
--- system/scripts/install-bundler.sh
+++ system/scripts/install-bundler.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o xtrace  # trace what gets executed
+set -o errexit # exit when a command fails.
+set -o nounset # exit when your script tries to use undeclared variables
 
 VERSION=1.10.6
 
