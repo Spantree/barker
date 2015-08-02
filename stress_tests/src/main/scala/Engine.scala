3cd9a86aca15719269824302c216708812c6be83
Cedric Hurst <cedric@spantree.net>
2015-08-01 21:53:16 -0500

    restructuring so that engine is the main class

---
+++ stress_tests/src/main/scala/Engine.scala
@@ -0,0 +1,15 @@
+import io.gatling.app.Gatling
+import io.gatling.core.config.GatlingPropertiesBuilder
+
+object Engine extends App {
+
+	val props = new GatlingPropertiesBuilder
+	props.disableCompiler
+	props.dataDirectory(IDEPathHelper.dataDirectory.toString)
+	props.resultsDirectory(IDEPathHelper.resultsDirectory.toString)
+	props.requestBodiesDirectory(IDEPathHelper.requestBodiesDirectory.toString)
+	props.binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString)
+	props.simulationClass(classOf[UserSimulation].getCanonicalName)
+
+	Gatling.fromMap(props.build)
+}
